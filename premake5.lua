77d08b0b847644a9aa6b274c2ab4fba6b9bede54
Qrikko <qrikko@gmail.com>
2022-02-03 22:08:07 +0100

    updated premake

--- premake5.lua
+++ premake5.lua
@@ -1,53 +1,52 @@
 project "cglm"
-    kind "staticLib"
-    language "C"
-    cdialect "C11"
-    staticruntime "on"
+	kind "staticLib"
+	language "C"
+	cdialect "C11"
+	staticruntime "on"
+	toolset "clang"
 
-    targetdir (engine_root .. "/lib/" .. outputdir)
-    objdir (engine_root .. "/bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir (engine_root .. "/lib/" .. outputdir)
+	objdir (engine_root .. "/bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files {
-        "src/euler.c",
-        "src/affine.c",
-        "src/io.c",
-        "src/quat.c",
-        "src/cam.c",
-        "src/vec2.c",
-        "src/vec3.c",
-        "src/vec4.c",
-        "src/mat2.c",
-        "src/mat3.c",
-        "src/mat4.c",
-        "src/plane.c",
-        "src/frustum.c",
-        "src/box.c",
-        "src/project.c",
-        "src/sphere.c",
-        "src/ease.c",
-        "src/curve.c",
-        "src/bezier.c",
-        "src/ray.c",
-        "src/affine2d.c",
-        "src/clipspace/persp_lh_zo.c",
-        "src/clipspace/persp_rh_zo.c",
-        "src/clipspace/persp_lh_no.c",
-        "src/clipspace/persp_rh_no.c",
-        "src/clipspace/ortho_lh_zo.c",
-        "src/clipspace/ortho_rh_zo.c",
-        "src/clipspace/ortho_lh_no.c",
-        "src/clipspace/ortho_rh_no.c",
-        "src/clipspace/view_lh_zo.c",
-        "src/clipspace/view_rh_zo.c",
-        "src/clipspace/view_lh_no.c",
-        "src/clipspace/view_rh_no.c"
-    }
+	files {
+		"src/euler.c",
+		"src/affine.c",
+		"src/io.c",
+		"src/quat.c",
+		"src/cam.c",
+		"src/vec2.c",
+		"src/vec3.c",
+		"src/vec4.c",
+		"src/mat2.c",
+		"src/mat3.c",
+		"src/mat4.c",
+		"src/plane.c",
+		"src/frustum.c",
+		"src/box.c",
+		"src/project.c",
+		"src/sphere.c",
+		"src/ease.c",
+		"src/curve.c",
+		"src/bezier.c",
+		"src/ray.c",
+		"src/affine2d.c",
+		"src/clipspace/persp_lh_zo.c",
+		"src/clipspace/persp_rh_zo.c",
+		"src/clipspace/persp_lh_no.c",
+		"src/clipspace/persp_rh_no.c",
+		"src/clipspace/ortho_lh_zo.c",
+		"src/clipspace/ortho_rh_zo.c",
+		"src/clipspace/ortho_lh_no.c",
+		"src/clipspace/ortho_rh_no.c",
+		"src/clipspace/view_lh_zo.c",
+		"src/clipspace/view_rh_zo.c",
+		"src/clipspace/view_lh_no.c",
+		"src/clipspace/view_rh_no.c"
+	}
 
-    filter "configurations:Debug"
-        runtime "Debug"
-        symbols "on"
-    filter "configurations:Release"
-        runtime "Release"
-        optimize "on"
-
-
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
